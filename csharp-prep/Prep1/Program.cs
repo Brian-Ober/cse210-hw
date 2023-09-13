1eb5760e3de63da5bc135e24491af72ab44ac96a
Brian-Ober <obe18001@byui.edu>
2023-09-13 10:35:28 -0600

    Did prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep1 World!"); 
+
+        //string interpolation
+        int myInt =5;
+        Console.WriteLine("my int = " + myInt);
+        Console.WriteLine($"my int = {myInt}");
+
+        //Readline
+        Console.Write("What is your name? ");
+        string name =Console.ReadLine();
+        Console.WriteLine($"name = {name}");
+
+        //Conditionals
+        if (name == "Brian")
+        {
+            Console.WriteLine("Hey that's me");
+        }
+        else 
+        {
+            Console.WriteLine($"Hi there, {name}");
+
+        }
+
+        Console.WriteLine(" ");
+
+
+        Console.Write("What is your first name? ");
+        string fname =Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname =Console.ReadLine();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
